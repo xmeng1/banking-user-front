f80cb41c20b8835b1967193399dbb59afd156794
Xin Meng <xin.meng@deepnetsecurity.com>
2017-11-02 22:42:31 +0000

    add hibernate support
    1. OneToMany
    mappedBy  cascade  fetchType
    2. ManyToOne

--- src/main/java/science/mengxin/banking/userfront/domain/PrimaryAccount.java
+++ src/main/java/science/mengxin/banking/userfront/domain/PrimaryAccount.java
@@ -1,13 +1,28 @@
 package science.mengxin.banking.userfront.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.math.BigDecimal;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+@Entity
 public class PrimaryAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private int accountNumber;
     // why not use double
     private BigDecimal accountBalance;
+
+    @OneToMany(mappedBy = "primaryAccount", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
     private List<PrimaryTransaction> primaryTransactionList;
 
     public Long getId() {
