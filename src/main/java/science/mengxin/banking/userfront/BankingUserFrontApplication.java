ed4c613c089d5fb6a36995372ced42ff065d504a
Xin Meng <xin.meng@deepnetsecurity.com>
2017-11-02 08:22:21 +0000

    Initial commit

---
+++ src/main/java/science/mengxin/banking/userfront/BankingUserFrontApplication.java
@@ -0,0 +1,12 @@
+package science.mengxin.banking.userfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingUserFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingUserFrontApplication.class, args);
+	}
+}
---
+++ src/test/java/science/mengxin/banking/userfront/BankingUserFrontApplicationTests.java
@@ -0,0 +1,16 @@
+package science.mengxin.banking.userfront;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankingUserFrontApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
