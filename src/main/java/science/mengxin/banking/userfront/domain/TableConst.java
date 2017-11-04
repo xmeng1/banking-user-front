2da6e5d36dc5c23c533e17fc5cff15cf3e775c11
Xin Meng <xin.meng@deepnetsecurity.com>
2017-11-04 18:39:17 +0000

    try to add const for the column name

---
+++ src/main/java/science/mengxin/banking/userfront/domain/TableConst.java
@@ -0,0 +1,16 @@
+package science.mengxin.banking.userfront.domain;
+
+/**
+ * User:    mengxin
+ * Date:    04/11/2017
+ * Project: banking-user-front
+ * Package: science.mengxin.banking.userfront.domain
+ * Description: TableConst.
+ *
+ * @author mengxin
+ * @version 1.0
+ */
+public class TableConst {
+    public static String primaryAccountIdColumnName = "primary_account_id";
+    public static String userIdColumnName = "userId";
+}
