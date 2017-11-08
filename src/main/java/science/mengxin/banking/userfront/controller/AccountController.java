5fb474c85b66ac77373610b59b3b88ea54b072ad
Xin Meng <xin.meng@deepnetsecurity.com>
2017-11-08 23:07:35 +0000

    024-add primary and saving account template page.

--- src/main/java/science/mengxin/banking/userfront/controller/AccountController.java
+++ src/main/java/science/mengxin/banking/userfront/controller/AccountController.java
@@ -1,7 +1,14 @@
 package science.mengxin.banking.userfront.controller;
 
+import java.security.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import science.mengxin.banking.userfront.domain.PrimaryAccount;
+import science.mengxin.banking.userfront.domain.SavingsAccount;
+import science.mengxin.banking.userfront.domain.User;
+import science.mengxin.banking.userfront.service.UserService;
 
 /**
  * User:    mengxin
@@ -18,13 +25,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/account")
 public class AccountController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/primaryAccount")
-    public String primaryAccount() {
+    public String primaryAccount(Model model, Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+
+        model.addAttribute("primaryAccount", primaryAccount);
         return "primaryAccount";
     }
 
     @RequestMapping("/savingsAccount")
-    public String savingsAccount() {
+    public String savingsAccount(Model model, Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        SavingsAccount savingsAccount = user.getSavingsAccount();
+
+        model.addAttribute("savingsAccount", savingsAccount);
         return "savingsAccount";
     }
 }
